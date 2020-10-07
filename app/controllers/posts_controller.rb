3c8d9c183ea822c0cb04a05b32126ec6d639977a
yuyake-rs <71796419+yuyake-rs@users.noreply.github.com>
2020-10-07 19:31:30 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,10 @@ class PostsController < ApplicationController
     #↓@posts = Post.all  # 1番目のレコードを@postに代入
     @posts = Post.all.order(id: "DESC")
   end
-  
-
   def create
     Post.create(content: params[:content])
     redirect_to action: :index
   end
-  
   def checked
     post = Post.find(params[:id])
     if post.checked 
@@ -17,7 +14,6 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
-
     item = Post.find(params[:id])
     render json: { post: item }
   end
